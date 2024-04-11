67004e4161c9cb855b4adb41d3f1df407d822653
ian <i.olmedo@alumno.um.edu.ar>
2024-04-11 09:52:12 -0300

    coment y config

--- backend/main/resources/comentario.py
+++ backend/main/resources/comentario.py
@@ -1,5 +1,8 @@
 from flask_restful import Resource
 from flask import request
+from .. import db
+from main.models import ComentarioModel
+
 
 COMENTARIOS = {
     1:{"cliente":"Nahuel Vicente", "libro":"Programación para tontos", "fecha":"27/04/2024", "mensaje":"Muy bueno"},
@@ -8,8 +11,11 @@ COMENTARIOS = {
 
 class Comentarios(Resource):
     def get(self):
-        return COMENTARIOS
-    
+        """return COMENTARIOS"""
+        comentario = db.session.query(ComentarioModel).get_or_404(id)
+        return comentario.to_json()
+
+
     def post(self):
         prestamo = request.get_json()
         id = int(max(COMENTARIOS.keys()))+1
