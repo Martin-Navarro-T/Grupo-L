d09f5bcf2d534dc1b136c05a8f19de561cc9d83b
ian <i.olmedo@alumno.um.edu.ar>
2024-04-11 12:03:29 -0300

    sin comentarios

--- backend/main/models/__init__.py
+++ backend/main/models/__init__.py
@@ -1,5 +1,4 @@
 from .usuario import Usuario as UsuarioModel
 from .valoracion import Valoraciones as ValoracionesModel
-from .comentario import Comentario as ComentarioModel
 from .configuracion import Configuracion as ConfiguracionModel
 from .configuracion import Configuraciones as ConfiguracionesModel
