859f48e0bda811601334a6bc6925a39184409a55
Martin_Navarro_T <mt.navarro@alumno.um.edu.ar>
2024-04-11 11:42:30 -0300

    Correción comentario y Ramas

--- backend/main/models/__init__.py
+++ backend/main/models/__init__.py
@@ -1,6 +1,7 @@
 from .usuario import Usuario as UsuarioModel
 from .valoracion import Valoraciones as ValoracionesModel
-from .prestamo import Prestamo as PrestamoModel
-from .libro import Libro as LibroModel
+from .comentario import Comentario as ComentarioModel
 from .configuracion import Configuracion as ConfiguracionModel
 from .configuracion import Configuraciones as ConfiguracionesModel
+from .prestamo import Prestamo as PrestamoModel
+from .libro import Libro as LibroModel
