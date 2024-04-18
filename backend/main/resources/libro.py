dda87ac22d4ed45a5d62bdafd10b88e405a649fd
Camila-Choque <m.choque@alumno.um.edu.ar>
2024-04-18 11:52:44 -0300

    Libros r/m

--- backend/main/resources/libro.py
+++ backend/main/resources/libro.py
@@ -1,45 +1,46 @@
 from flask_restful import Resource
-from flask import request
+from flask import request, jsonify
+from .. import db
+from main.models import LibroModel
 
-LIBROS = {
-    1:{"titulo":"Fisica II", "editorial":"planeta", "autor":"Ian Lopez"},
-    2:{"titulo":"Matematica", "editorial":"hp.edu", "autor":"Martin Olmedo"},
-}
 
 class Libro(Resource):
     def get(self,id):
-        if int(id) in LIBROS:
-            return LIBROS[int(id)] 
-        else:
-            return "No existe ese libro", 404 
+    
+        libro = db.session.query(LibroModel).get_or_404(id)
+        return libro.to_json(), 201 
         
     def put(self,id):
-        if int(id) in LIBROS:
-            libro = LIBROS[int(id)]
-            data = request.get_json()
-            libro.update(data)
-            return "El libro fue actualizado con exito"
-        else:
-            "No existe el libro", 404
+        libro = db.session.query(LibroModel).get_or_404(id)
+        data = request.get_json().items()
+        for key, value in data:
+            setattr(libro, key, value)
+        db.session.add(libro)
+        db.session.commit()
+        return libro.to_json() , 201
 
 
     def delete(self,id):
-        if int(id) in LIBROS:
-            del LIBROS[int(id)]
-            return "El libro fue eliminado con exito", 204 
-        else: 
-            return "No existe el libro",404
-        
+        libro = db.session.query(LibroModel).get_or_404(id)
+        db.session.delete(libro)
+        db.session.commit()
+        return "El libro fue eliminado correctamente", 204
+    
+    
+    
+    
 
 class Libros(Resource):
     def get(self):
-        return LIBROS
+        libros = db.session.query(LibroModel).all()
+        return jsonify([libro.to_json() for libro in libros])
 
     def post(self):
-        libro = request.get_json()
-        id = int(max(LIBROS.keys()))+1
-        LIBROS[id] = libro
-        return LIBROS[id]
+        libro = LibroModel.from_json(request.get_json())
+        db.session.add(libro)
+        db.session.commit()
+        return libro.to_json(), 201
+
 
 
 
